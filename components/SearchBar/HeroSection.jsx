c24f0f519f780e9fbf9d18f25883db04b6b5293d
Kabita <gautamkabita284@gmail.com>
2024-02-25 11:23:14 +0545

    Worked on z-index of searchbar and colors in filter

--- components/SearchBar/HeroSection.jsx
+++ components/SearchBar/HeroSection.jsx
@@ -5,10 +5,10 @@ const HeroSection = () => {
     <div className="relative z-0">
       <div className="container mx-auto px-6 relative z-10 flex items-center justify-center h-full ">
         <div className={`max-w-4xl text-center`}>
-          <h1 className="md:text-4xl font-bold text-[#C7C6C1] mb-2 only:leading-tight">
+          <h1 className="md:text-4xl font-bold text-white mb-2 only:leading-tight">
             Unlock Your Stay: Book with Blockchain & NFTs!
           </h1>
-          <p className="text-2xl mb-40 text-[#C7C6C1]">
+          <p className="text-2xl mb-40 text-white">
             Step into the luxury with curated experiences, exclusive access, and unique digital assets. Own a piece of the high life with blockchain-powered ownership.
           </p>
         </div>
