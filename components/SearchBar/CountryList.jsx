8ff0547f2979aa03f239e15bfe2cbdfdb82d07a3
Kabita <gautamkabita284@gmail.com>
2024-03-31 16:10:51 +0545

    Fixed build error

--- components/SearchBar/CountryList.jsx
+++ components/SearchBar/CountryList.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from 'react';
-import { useState } from 'react';
+import React from 'react';
 import hotelData from '../../data/CountryList.json';
 
 function CountryList({ setSelectedLocation, setIsLocationDropdownOpen }) {
