7c237e1e0a3297233fcdb6277436ecebac38c7a7
apple <biishnuthapa@gmail.com>
2024-04-02 14:38:50 +0545

    added $

--- components/RoomList.jsx
+++ components/RoomList.jsx
@@ -61,7 +61,7 @@ const RoomList = ({ apartmentId }) => {
                 scope="col"
                 className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
               >
-                Price
+                Price($)
               </th>
               <th
                 scope="col"
